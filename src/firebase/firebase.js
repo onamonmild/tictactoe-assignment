2c111ca864d1f3f80611be3e79b1a32f23c0ab4f
onamonmild <onamon_mild@hotmail.com>
2024-11-07 23:30:09 +0700

    Add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWrWMtP8Uygp1C-F98MpqCHacDNhsYEBg",
+  authDomain: "tictactoe-assignment.firebaseapp.com",
+  projectId: "tictactoe-assignment",
+  storageBucket: "tictactoe-assignment.firebasestorage.app",
+  messagingSenderId: "484911321028",
+  appId: "1:484911321028:web:8683948a71837f44785e91"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
